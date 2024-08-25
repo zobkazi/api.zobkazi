516814c7d7b4e329ca416e3192b9465a70adb077
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-08-25 23:56:14 +0600

    getBlogs helpers

--- src/api/handlers/index.js
+++ src/api/handlers/index.js
@@ -7,6 +7,35 @@ const formatResponse = (status, message, data = null) => {
   };
 };
 
+const getBlogs = async (request, h) => {
+  const blogs = await Blog.find();
+  return h
+    .response({
+      message: "blogs fetched successfully",
+      data: blogs,
+      success: true,
+      status: 200,
+      error: null,
+    })
+    .code(200);
+};
+
+const getBlogById = async (request, h) => {
+  const blog = await Blog.findById(request.params.id);
+  if (!blog) {
+    throw Boom.notFound("blog not found");
+  }
+  return h
+    .response({
+      message: "blog fetched successfully",
+      data: blog,
+      success: true,
+      status: 200,
+      error: null,
+    })
+    .code(200);
+};
+
 module.exports = {
   formatResponse,
 };
