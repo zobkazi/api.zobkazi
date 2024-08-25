52f62339dbb13feae321ffe682067eef63c914a9
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-08-25 23:52:41 +0600

    delete blog by id

--- src/api/modules/blog/blog.controller.js
+++ src/api/modules/blog/blog.controller.js
@@ -59,6 +59,24 @@ const getBlogById = async (request, h) => {
     .code(200);
 };
 
+// delete blog controller
+
+const deleteBlog = async (request, h) => {
+  const blog = await Blog.findByIdAndDelete(request.params.id);
+  if (!blog) {
+    throw Boom.notFound("blog not found");
+  }
+  return h
+    .response({
+      message: "blog deleted successfully",
+      data: blog,
+      success: true,
+      status: 200,
+      error: null,
+    })
+    .code(200);
+};
+
 // create blog controller
 
 module.exports = {
