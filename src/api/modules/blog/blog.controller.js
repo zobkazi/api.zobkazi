80ff7578ab33669472ceacb41dbb43bf67d460b6
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-08-25 23:52:13 +0600

    get by id

--- src/api/modules/blog/blog.controller.js
+++ src/api/modules/blog/blog.controller.js
@@ -26,7 +26,38 @@ const createBlog = async (request, h) => {
     .code(200);
 };
 
-// create blog controller
+// get all blogs controller
+
+const getBlogs = async (request, h) => {
+  const blogs = await Blog.find();
+  return h
+    .response({
+      message: "blogs fetched successfully",
+      data: blogs,
+      success: true,
+      status: 200,
+      error: null,
+    })
+    .code(200);
+};
+
+// get blog by id controller
+
+const getBlogById = async (request, h) => {
+  const blog = await Blog.findById(request.params.id);
+  if (!blog) {
+    throw Boom.notFound("blog not found");
+  }
+  return h
+    .response({
+      message: "blog fetched successfully",
+      data: blog,
+      success: true,
+      status: 200,
+      error: null,
+    })
+    .code(200);
+};
 
 // create blog controller
 
