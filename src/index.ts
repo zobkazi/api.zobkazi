a4e91de0c919d5c363b7c35c49f3d192004bfa6d
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-02-23 22:38:14 +0600

    used this product

--- src/index.ts
+++ src/index.ts
@@ -6,25 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 })
 
-app.get("/api", (req, res) => {
-  res.status(200).json({
-    message: "Hello World!",
-    status: 200,
-    success: true,
-    data: null
-  })
-})
-
-app.get("/no", (req, res) => {
-  res.status(200).json({
-    message: "remote: Resolving deltas: 100% (2/2), completed with 2 local objects.",
-    status: 200,
-    success: true,
-    data: "remote: Resolving deltas: 100% (2/2), completed with 2 local objects."
-  })
-})
-
-
 
 export default app;
 
